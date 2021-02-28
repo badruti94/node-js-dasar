13eb6cfda368b8f79ac01f3d8547e6463f67188c
Badru <badruakfm@gmail.com>
2021-02-28 17:27:55 +0700

    Tutorial Node Js Dasar ( 16 ) - Operasi database My SQL

--- index.js
+++ index.js
@@ -2,29 +2,85 @@ var http = require('http')
 var url = require('url')
 var router = require('routes')()
 var view = require('swig')
+var mysql = require('mysql')
+const {
+    RSA_NO_PADDING
+} = require('constants')
 
-router.addRoute('/', function(req,res){
-    var html = view.compileFile('./template/index.html')({
-        title : 'Index Page from Rizal Afani'
-    })
-
-    res.writeHead(200, {'Content-Type' : 'text/html'})
-    res.end(html)
-});
-
-router.addRoute('/contact', function(req, res){
-    var html = view.compileFile('./template/contact.html')()
-    res.writeHead(200, {'Content-Type' : 'text/html'})
-    res.end(html)
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    database: 'sia',
+    user: 'root',
+    password: ''
 })
 
-http.createServer(function(req, res){
+router.addRoute('/', function (req, res) {
+    connection.query("select * from mahasiswa", function (err, rows, field) {
+        if (err) throw err;
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(JSON.stringify(rows))
+    })
+})
+
+router.addRoute('/insert', function (req, res) {
+    connection.query("insert into mahasiswa set ?", {
+        no_induk: '1110100604',
+        nama: 'Mahrusssah',
+        alamat: 'Genteng'
+    }, function (err, field) {
+        if (err) throw err;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(field.affectedRows + " Affected Rows")
+    })
+})
+
+
+
+router.addRoute('/update', function (req, res) {
+    connection.query("update mahasiswa set ? where ?", [{
+            nama: 'Mahrusssahroni'
+        },
+        {
+            no_induk: '1110100604'
+        }
+    ], function (err, fields) {
+        if (err) throw err
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(fields.changeRows + " Rows Updated");
+    })
+})
+
+router.addRoute('/delete', function (req, res) {
+    connection.query("delete from mahasiswa where ?", {
+        no_induk: "1110100604"
+    }, function (err, fields) {
+        if (err) throw err;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(fields.affectedRows + " Rows Deleted")
+    })
+})
+
+http.createServer(function (req, res) {
     var path = url.parse(req.url).pathname;
     var match = router.match(path)
-    if(match){
+    if (match) {
         match.fn(req, res)
-    }else{
-        res.writeHead(404, {'Content-Type' : 'text/html'})
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        })
         res.end('Page not found')
     }
 }).listen(8000)
